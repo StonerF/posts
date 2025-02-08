a8243c1cbd8da7cd4c07721cf92605edab485df8
StonerF <roman.sumanov@math.msu.ru>
2025-02-08 19:33:26 +0300

    Add GraphQl v.1

---
+++ graph/resolver.go
@@ -0,0 +1,27 @@
+package graph
+
+//go:generate go run github.com/99designs/gqlgen generate
+
+import (
+	"sync"
+
+	"github.com/StonerF/posts/graph/model"
+	"github.com/StonerF/posts/internal/storage"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Resolver struct {
+	Repo             storage.Repository
+	CommentObservers map[string]chan *model.Comment
+	mu               sync.Mutex
+}
+
+func NewResolver(Repo storage.Repository) *Resolver {
+	return &Resolver{
+		Repo:             Repo,
+		CommentObservers: make(map[string]chan *model.Comment),
+	}
+}
